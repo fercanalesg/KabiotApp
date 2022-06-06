357bd4ebd612a19885e531004a1ceda8d3352b6c
fercanalesg <lufercanalesg@gmail.com>
2022-06-06 12:49:19 -0500

    added education/experience

---
+++ Desktop/Hackathon Portfolio/project-elegant-elephant-main/app/__init__.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask, render_template, request
+from dotenv import load_dotenv
+
+load_dotenv()
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', title="Home", url=os.getenv("URL"))
+
+@app.route('/professionalinfo')
+def professionInfo():
+    return render_template('prof.html', title="Education/Experience", url=os.getenv("URL"))
+
+@app.route('/hobbies')
+def portfolio():
+    return render_template('hobbies.html', title="Hobbies", url=os.getenv("URL"))
